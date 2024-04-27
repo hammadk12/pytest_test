d9ed948d76a9226125e92156ec2c1523db22a377
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-27 07:47:41 -0400

    initial commit - testing out pytest

---
+++ my_test.py
@@ -0,0 +1,10 @@
+
+def main():
+    name = input("What's your age? ")
+    print(age(name))
+
+def age(to):
+    return f"age: {to}"
+
+if __name__ == "__main__":
+    main()
---
+++ testing.py
@@ -0,0 +1,5 @@
+from my_test import age
+
+def testing_age():
+    assert age("18") == "age: 18"
+    assert age("21") == "age: 21"
